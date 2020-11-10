77d83faa5f8c82f6bdd2eb17c640e14d1a08541a
Michael <hiebertmd@gmail.com>
2020-11-10 18:33:14 -0500

    Implemented Dark Chess

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from minichess.games.dark.board import DarkChessBoard
 from minichess.games.rifle.board import RifleChessBoard
 from minichess.games.abstract.board import AbstractBoardStatus
 from minichess.players.gardner import RandomPlayer
@@ -7,12 +8,14 @@ from minichess.games.gardner.action import GardnerChessAction
 if __name__ == "__main__":
     # g = GardnerChessBoard()
     r = RifleChessBoard()
+    d = DarkChessBoard()
     p = RandomPlayer()
 
-    game = r
+    game = d
 
     while game.status == AbstractBoardStatus.ONGOING:
         print(game)
+        # print(game.no_fog_board())
 
         actions = game.legal_actions()
 
@@ -20,6 +23,7 @@ if __name__ == "__main__":
         #     print(action)
         
         proposed = p.propose_action(game, None, game.legal_action_mask())
+        print(game.state_vector())
 
         input()
         proposed = GardnerChessAction.decode(proposed, game)
